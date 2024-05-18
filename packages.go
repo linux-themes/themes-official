ecec60eefe6f1086bef70d91a68fb2b659e09771
jordan <sudo.sendmail.jordan@gmail.com>
2024-05-18 11:23:44 -0700

    Update packages.go

--- packages.go
+++ packages.go
@@ -8,6 +8,6 @@ type Package struct {
 }
 
 var packages_offical = map[int]Package{
-	1: {"mint", "https://", "icons", false},
+	1: {"mint", "https://github.com/linux-themes/themes/raw/main/icons/mint.tar.xz", "icons", false},
 	2: {"marble-shell", "https://github.com/linux-themes/themes/raw/main/themes/marble-shell.tar.gz", "themes", false},
 }
